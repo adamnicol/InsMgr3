7be4911e1070bf52d7023c5b300d5a81753ec65d
adamnicol <adamnicol85@gmail.com>
2017-03-15 21:48:20 +0000

    Added project for common code

---
+++ InsMgr3.Common/Extensions/BitmapExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace InsMgr3.Common.Extensions
+{
+    public static class BitmapExtensions
+    {
+        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+        {
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Bmp);
+                stream.Position = 0;
+
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+
+                return image;
+            }
+        }
+    }
+}
